232726db78a954b1b8debe4767bfb4ec3723e047
Martinsr99 <silesreche.martin@gmail.com>
2024-04-29 23:42:30 +0200

    feat: Fix first version

--- src/app/result-view/result-view.component.ts
+++ src/app/result-view/result-view.component.ts
@@ -11,21 +11,28 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./result-view.component.scss'],
 })
 export class ResultViewComponent implements OnDestroy {
-  results: string[] = [];
+  images: string[] = [];
   private subscription: Subscription;
 
   constructor(private resultsService: ResultsService) {
-    this.subscription = this.resultsService.currentResults.subscribe(results => {
-      this.results = results;
+    this.subscription = this.resultsService.currentResults.subscribe({
+      next: (data:any) => {
+        this.images = Array.isArray(data.images) ? data.images : [];
+        console.log('Results updated:', this.images);
+      },
+      error: (err) => {
+        console.error('Error receiving results:', err);
+        this.images = [];  // Reinicializa a un array vacío en caso de error.
+      }
     });
+    
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe(); // Limpiar la suscripción
+    this.subscription.unsubscribe(); // Clean up the subscription
   }
 
   downloadImage(imageUrl: string): void {
     window.open(imageUrl, '_blank');
   }
-  
 }
