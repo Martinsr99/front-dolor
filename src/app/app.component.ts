3c698a1f93472de8e1b09cdbc220d633d68b8a41
Martinsr99 <silesreche.martin@gmail.com>
2024-05-01 00:13:40 +0200

    feat: add version header version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { ResultViewComponent } from './result-view/result-view.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FileUploadComponent,ResultViewComponent,HttpClientModule],
+  imports: [RouterOutlet,FileUploadComponent,ResultViewComponent,HttpClientModule,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
